379f70c7eae75dc4fa7cfe2494f578b357fa1b0b
LeonRemy <leonremyy2308@gmail.com>
2026-06-07 12:55:40 +0700

    thêm cấu trúc cây

--- BTT14.c
+++ BTT14.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+}Node;
 
+Node* Create_Node(int data) {
+    Node* n = (Node*)malloc(sizeof(Node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+}
+
+typedef struct {
+    Node* root;
+}Tree;
 
